6c578bb1f597d087da567428293cfed1347fb384
Elie Mynnet <e4cohen@enib.fr>
2019-12-24 11:11:52 +0100

    clean useless code and other little things

--- src/navigation/navigation.ino
+++ src/navigation/navigation.ino
@@ -10,12 +10,6 @@ File myFile;
 Servo barre;
 Servo aile;
 
-// Variables utilisés par la routine d'interruption qui remplace pulseIn()   (reception radio)
-volatile unsigned long LastPulseTime_ch3;
-int duration;
-#define ch3Pin 5
-
-int course_aileron = 10;
 int pres_VMG = 40;
 int portant_VMG = 150;
 
@@ -24,17 +18,9 @@ int pos3 = 80 + 10;
 
 int ledPin = 13;                  // LED test pin
 int current_mode = 0;
-int last_mode = 0;
-int i = 0;
-int ch3;
-int ch6;
-int ch5;
 int angle_regulateur;
-int angle1 = 0;
-int precision = 0;
 int cap_moyen;
 boolean first_loop = true;
-boolean led_state = true;
 boolean hors_couloir = true;
 
 // liste points GPS
@@ -45,7 +31,6 @@ const float wp_lon[] = {-4.615716, -4.614247};//{-4.614617, -4.615505};//
 unsigned int index_wpt = 0;              // index dans la liste des wpt
 int seuil_valid_wpt = 25;
 
-int estim_vent = -1;            // angle estimé du vent. Si -1 incertitude trop importante
 int tab_cap[10];
 unsigned int index_tab_cap = 0;
 int nouvel_angle_regulateur;                 // Consigne du régulateur pour atteindre le waypoint
@@ -60,13 +45,14 @@ unsigned long interval_calcul = 10000;
 
 // variables globales pour le data logger
 unsigned long interval_datalogging = 1000;//1000;
-String var_name_log[] = {"Heures","Minutes", "Secondes","HDOP", "Vitesse", "Cap", "Angle_regulateur", "Pos_aile", "Cap_moy", "precision", "Nb_satellites", "Latittude", "Longitude","Lat_next_point","Lon_next_point","Wpt_angle", "Wpt_dst","ecart_axe","Presence_couloir", "Mode","Index_wpt", "Commentaire"};
+String var_name_log[] = {"Heures","Minutes", "Secondes","HDOP", "Vitesse", "Cap", "Angle_regulateur", "Pos_aile", "Cap_moy", "Nb_satellites", "Latittude", "Longitude","Lat_next_point","Lon_next_point","Wpt_angle", "Wpt_dst","ecart_axe","Presence_couloir", "Mode","Index_wpt", "Commentaire"};
 int buf[sizeof(var_name_log)];
 
 int index_buffer_lignes = 0;
 int taille_buffer_lignes = 10;
 String lines_buffer[10];
 
+// GPS data
 long lat, lon;
 unsigned long fix_age, time, date, speed, course;
 unsigned long chars;
@@ -79,7 +65,7 @@ void datalog(String var_name, int value){
     if (var_name == "push") {
         // 4) Sur appel du mot clef "push", copier le buffer dans la carte sd. Et clear buffer.
         String line;
-        for (unsigned int i = 0; i < sizeof(var_name_log)/sizeof(var_name_log[0]); i++){
+        for (unsigned int i = 0; i < sizeof(var_name_log)/sizeof(var_name_log[0]) - 1; i++){
             line += buf[i];
             line += ";";
         }
@@ -147,9 +133,6 @@ int filtrage_cap(int cap_instant){
     x_cap += cos(radians(tab_cap[i]));
     y_cap += sin(radians(tab_cap[i]));
   }
-
-  //precision = 0;//x_cap*x_cap + y_cap*y_cap;   // sur 10 mesures, la norme va être entre 0 (tout les angles sont opposés) et 10 (le cap venait toujours du même angle).
-  //datalog("precision", precision);
 
   if (x_cap != 0){
     cap = (int)(180.0*atan(y_cap/x_cap)/PI);
@@ -227,15 +210,6 @@ void reglage_aile_auto(int angle3){
   }
 }
 
-void mode_semi_auto(int angle2){
-  /////////////////////////////////////
-  //        Mode semi auto           //
-  /////////////////////////////////////
-  estim_vent = -1;
-  commande_barre(angle2);
-  reglage_aile_auto(angle2);
-}
-
 void mode_autonome(){
   /////////////////////////////////////
   //        Mode full auto           //
@@ -250,59 +224,56 @@ void mode_autonome(){
 
   if (millis() - timer2 > interval_calcul){  // calcul toutes les 10 secondes
     timer2 = millis();
-    boolean virement_prevu = false;
-    if (true){ //precision >= 6){  // cap stabilisé sur une trajectoire presque rectiligne
-      int ecart_route = analyse_vent(cap_moyen, angleToWaypoint);
-      nouvel_angle_regulateur = angle_regulateur + ecart_route;
-      if (nouvel_angle_regulateur > 360){
-        nouvel_angle_regulateur -= 360;
-      }
-
-      if (nouvel_angle_regulateur > (360-pres_VMG) || nouvel_angle_regulateur < pres_VMG){ // On bloque les positions face au vent
-        if (angle_regulateur < 180){
-          nouvel_angle_regulateur = pres_VMG;    // on louvoie en continuant sur le bord en cours
-        }
-        else {
-          nouvel_angle_regulateur = 360 - pres_VMG;    // on louvoie en continuant sur le bord en cours
-        }
-
-      }
-
-      if (nouvel_angle_regulateur > 150 && nouvel_angle_regulateur < 210){  // on bloque le vent arrière pour faire du largue
-         if (angle_regulateur < 180){
-          nouvel_angle_regulateur = portant_VMG;    // on louvoie en continuant sur le bord en cours
-        }
-        else {
-          nouvel_angle_regulateur = 360 - portant_VMG;    // on louvoie en continuant sur le bord en cours
-        }
-      }
-
-      if (angle_regulateur != pres_VMG && angle_regulateur != 360 - pres_VMG){  // si on n'est pas au près :
-        if (nouvel_angle_regulateur > 0 && nouvel_angle_regulateur <= 180 && angle_regulateur > 180 && angle_regulateur <= 360){  // On passe de tribord amure à babord amure
-          nouvel_angle_regulateur = 360-pres_VMG;
-        }
-        if (angle_regulateur > 0 && angle_regulateur <= 180 && nouvel_angle_regulateur > 180 && nouvel_angle_regulateur <= 360){  // On passe de babord amure à tribord amure
-          nouvel_angle_regulateur = pres_VMG;
-        }
-      }
-
-      // Test du couloir
-      boolean precence_couloir = test_couloir(60); //test_couloir() retourne true si le bateau est dans le couloir
-      datalog("Presence_couloir",precence_couloir);
-      if (not precence_couloir){
-        if (not hors_couloir){ // si on n'était pas déjà hors couloir on change la consigne pour retourner vers le couloir.
-          hors_couloir = true;
-          nouvel_angle_regulateur = 360 - nouvel_angle_regulateur;
-        }
-      }
-      else {  // si on est dans le couloir :
-        hors_couloir = false;
-      }
-
-      // application des consignes calculées sur les servos
-      commande_barre(nouvel_angle_regulateur);
-      reglage_aile_auto(nouvel_angle_regulateur);
+    int ecart_route = analyse_vent(cap_moyen, angleToWaypoint);
+    nouvel_angle_regulateur = angle_regulateur + ecart_route;
+    if (nouvel_angle_regulateur > 360){
+      nouvel_angle_regulateur -= 360;
     }
+
+    if (nouvel_angle_regulateur > (360-pres_VMG) || nouvel_angle_regulateur < pres_VMG){ // On bloque les positions face au vent
+      if (angle_regulateur < 180){
+        nouvel_angle_regulateur = pres_VMG;    // on louvoie en continuant sur le bord en cours
+      }
+      else {
+        nouvel_angle_regulateur = 360 - pres_VMG;    // on louvoie en continuant sur le bord en cours
+      }
+
+    }
+
+    if (nouvel_angle_regulateur > 150 && nouvel_angle_regulateur < 210){  // on bloque le vent arrière pour faire du largue
+       if (angle_regulateur < 180){
+        nouvel_angle_regulateur = portant_VMG;    // on louvoie en continuant sur le bord en cours
+      }
+      else {
+        nouvel_angle_regulateur = 360 - portant_VMG;    // on louvoie en continuant sur le bord en cours
+      }
+    }
+
+    if (angle_regulateur != pres_VMG && angle_regulateur != 360 - pres_VMG){  // si on n'est pas au près :
+      if (nouvel_angle_regulateur > 0 && nouvel_angle_regulateur <= 180 && angle_regulateur > 180 && angle_regulateur <= 360){  // On passe de tribord amure à babord amure
+        nouvel_angle_regulateur = 360-pres_VMG;
+      }
+      if (angle_regulateur > 0 && angle_regulateur <= 180 && nouvel_angle_regulateur > 180 && nouvel_angle_regulateur <= 360){  // On passe de babord amure à tribord amure
+        nouvel_angle_regulateur = pres_VMG;
+      }
+    }
+
+    // Test du couloir
+    boolean precence_couloir = test_couloir(60); //test_couloir() retourne true si le bateau est dans le couloir
+    datalog("Presence_couloir",precence_couloir);
+    if (not precence_couloir){
+      if (not hors_couloir){ // si on n'était pas déjà hors couloir on change la consigne pour retourner vers le couloir.
+        hors_couloir = true;
+        nouvel_angle_regulateur = 360 - nouvel_angle_regulateur;
+      }
+    }
+    else {  // si on est dans le couloir :
+      hors_couloir = false;
+    }
+
+    // application des consignes calculées sur les servos
+    commande_barre(nouvel_angle_regulateur);
+    reglage_aile_auto(nouvel_angle_regulateur);
   }
 }
 
@@ -360,23 +331,16 @@ void setup() {
   Serial1.begin(9600);
 
   //switch on the led
-  pinMode(13, OUTPUT);       // Initialize LED pin
-  digitalWrite(13, HIGH);
-  delay(5000);
+  pinMode(ledPin, OUTPUT);       // Initialize LED pin
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
   
   // initialisation GPS
   GPS_SERIAL.begin(9600);
 
-  // declaration des ports
-  //pinMode(A16, INPUT);   // sortie du filtre passe bas lié à la PWM du switch C
-
-  // set interruption on ch3pin
-  pinMode(ch3Pin, INPUT);
-  attachInterrupt(ch3Pin, EchoPinISR_ch3, CHANGE);  // Pin 2 interrupt on any change
-
   // init servo
-  barre.attach(3);
-  aile.attach(2);
+  barre.attach(4);
+  aile.attach(5);
 
   // init SD card
   if (!SD.begin(BUILTIN_SDCARD)) {
@@ -390,8 +354,8 @@ void setup() {
   datalog("init", 0);
 
   // switch off the led
-  delay(1000);
-  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
@@ -404,67 +368,15 @@ void loop() {
     datalog("Cap_moy",cap_moyen);
   }
 
-  //ch6 = LastPulseTime_ch6;  // Reception de la donnée switch "C" de la radiocommande (position d'aile)
-  ch3 = LastPulseTime_ch3; // Reception de la donnée de cap de la radiocommande
-  if (ch3 > 990 && ch3 < 2000){
-    angle1 = map(ch3, 990,2000,0,360)-180;   // 0 = face au vent
-    if (angle1 < 0){
-      angle1 += 360;
-    }
-  }
-
-  datalog("Index_wpt", index_wpt);
-  if (not(angle1 < 180 && angle1 > 160)){
-    current_mode = 0;
-    first_loop = true;
-  }
-  else {
-    current_mode = 1;
-  }
-
-  if (current_mode == 1){
-    datalog("Mode", 1);
-    mode_autonome();
-  }
-  else if (current_mode == 0){
-    datalog("Mode", 0);
-    mode_semi_auto(angle1);
-  }
-
-  int c = GPS_SERIAL.read();
-  if (gps.encode(c)){
-    if (millis() - timer4 > 1000 ) {  // témoin visuel sur la carte d'acroche des satelites.
-      timer4 = millis();
-      led_state = not(led_state);
-    }
-    if (led_state){
-      digitalWrite(13, HIGH);
-    }
-    else {
-      digitalWrite(13, LOW);
-    }
-  }
+  datalog("Mode", 1);
+  mode_autonome();
+  
   // Cadencement du dataloggeur et informations complémentaires
   if (millis() - timer3 > interval_datalogging) {
     timer3 = millis();
     datalog("push", 0);
   }
 }
-
-// Routine d'interruption qui compte les impulsions de la radiocommande
-void EchoPinISR_ch3() {
-  static unsigned long startTime;
-  if (digitalRead(ch3Pin)) // Gone HIGH
-    startTime = micros();
-  else  // Gone LOW
-  LastPulseTime_ch3 = micros() - startTime;
-}
-
-
-
-
-
-
 
 
 
