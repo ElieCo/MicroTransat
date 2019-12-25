cc4850ebd62d79259842aa7620a26d6a4abc4feb
Elie Mynnet <e4cohen@enib.fr>
2019-12-25 20:46:54 +0100

    test mode_autonome

--- src/test/simulation.ino
+++ src/test/simulation.ino
@@ -1,6 +1,11 @@
 
 float simu_wind_direction;
 float simu_dt;
+float simu_waypoint_lat, simu_waypoint_lon = 0;
+
+void debug(String s){
+  Serial1.println(s);
+}
 
 void initSimuMove(float wind, float regulation, int dt){
   // wind in degrees
@@ -11,13 +16,51 @@ void initSimuMove(float wind, float regulation, int dt){
   angle_regulateur = regulation;
 }
 
+void initSimuPos(float _lat, float _lon){
+  debug(_lat);
+  lat = _lat*1000000;
+  debug(lat);
+  lon = _lon*1000000;
+  debug(lon);
+}
+
+void initSimuWaypoint(float wlat, float wlon){
+  simu_waypoint_lat = wlat;
+  simu_waypoint_lon = wlon;
+}
+
 void simuMove(){
   //simulate the movement and redefine the cap, speed, lat and lon
   
-  speed = 5.f;
+  
+  datalog("Vitesse",(int)(speed));
+  datalog("Cap",(int) course);
+  datalog("Latittude",(int)(lat));
+  datalog("Longitude",(int)(lon));
+
+  
+  speed = 0.2;
   course = float(simu_wind_direction + angle_regulateur);
   float distance = speed * simu_dt / 1000;
   float nlat, nlon;
-  get_new_point(lat, lon, course, distance, nlat, nlon);
+  get_new_point(lat/1000000, lon/1000000, course, distance, nlat, nlon);
+  lat = nlat*1000000;
+  lon = nlon*1000000;
+
+
+  Serial1.print("lat: ");
+  Serial1.println(lat);
+  Serial1.print("lon: ");
+  Serial1.println(lon);
+  Serial1.print("course: ");
+  Serial1.println(course);
+  Serial1.print("speed: ");
+  Serial1.println(speed);
+  Serial1.println("====================");
+  
+}
+
+float getSimuAngleToWaypoint(){
+  return angleToWaypoint = TinyGPS::course_to(lat/1000000, lon/1000000, simu_waypoint_lat, simu_waypoint_lon);
 }
 
--- src/test/test.ino
+++ src/test/test.ino
@@ -3,34 +3,48 @@
 bool first_test_loop;
 
 void setup(){
-  Serial1.begin(9600);
+
+  navSetup();
+  
+  // Serial1.begin(9600);
+  Serial1.println("");
   Serial1.println("======================");
   Serial1.println("=== HERE THE TESTS ===");
   Serial1.println("======================");
+
   first_test_loop = true;
 }
 
 void loop(){
   if(first_test_loop){
     // run tests only once
-    testNav_02();
-
-    first_test_loop = false;
+    // testNav_02();
   }
 
   // run tests in a loop
-  //testNav_01();
+  testNav_01();
+
+
+  first_test_loop = false;
+  delay(1000);
 }
 
 bool testNav_01(){
-  first_loop = false;
+  // first_loop = false;
   timer2 = 0;
 
-  if(first_test_loop)
-    initSimuMove(0, 90, 10000);
+  if(first_test_loop){
+    initSimuMove(0, 50, interval_calcul);
+    initSimuPos(47.731564, -3.393134);
+    initSimuWaypoint(47.732344, -3.395307);
+  }
   simuMove();
   
   cap_moyen = course;
+  angleToWaypoint = getSimuAngleToWaypoint();
+
+  mode_autonome();
+  datalog("push", 0);
 }
 
 bool testNav_02(){
