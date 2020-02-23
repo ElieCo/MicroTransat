e2c512230e09ef14414e12ffd924f67cb2e92031
ElieCo <elieco96@gmail.com>
2020-02-23 19:31:22 +0100

    update servoMotor class

--- src/navigation/10_ServoMotor.ino
+++ src/navigation/10_ServoMotor.ino
@@ -1,15 +1,12 @@
 
 
-class ServoMotor : public Manager
+class ServoMotor
 {
  public:
   
-  ServoMotor(String name, int pin){
-    m_name = name;
+  ServoMotor(int pin){
     m_pin = pin;
     m_servo.attach(m_pin);
-
-    m_runInterval = 2000;
   }
   ~ServoMotor(){}
 
