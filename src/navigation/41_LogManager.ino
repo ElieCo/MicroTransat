a8e3914498824551c0b7bc956f7654884ccf12be
ElieCo <elieco96@gmail.com>
2020-03-02 23:35:57 +0100

    implement log manager

---
+++ src/navigation/41_LogManager.ino
@@ -0,0 +1,39 @@
+
+
+class LogManager: public BaseManager
+{
+  ~LogManager(){}
+
+  void init(){
+    m_sd.initLog();
+    
+    std::map<string, string> data = m_db->getAllData();
+    String line = "";
+    for (std::map<string, string>::iterator it = data.begin(); it != data.end(); ++it){
+      line += it->first.c_str();
+      if (it != data.end()) {
+        line += ";";
+      }
+    }
+    m_sd.log(line);
+  }
+
+  void go(){
+    std::map<string, string> data = m_db->getAllData();
+    String line = "";
+    for (std::map<string, string>::iterator it = data.begin(); it != data.end(); ++it){
+      line += it->second.c_str();
+      if (it != data.end()) {
+        line += ";";
+      }
+    }
+    m_sd.log(line);
+  }
+
+  void stop() {
+    m_sd.stop();
+  }
+
+  SDcard m_sd;
+};
+
