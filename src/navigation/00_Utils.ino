b9c362659ecdaf37c9b31b36eeafea33eb7cfff7
ElieCo <elieco96@gmail.com>
2020-03-07 19:02:33 +0100

    add usefull functions

--- src/navigation/00_Utils.ino
+++ src/navigation/00_Utils.ino
@@ -14,3 +14,17 @@ void print(String s1, String s2){
   serialDebug.println(s2);
 }
 
+float from0to360(float &angle){
+  return limit_angle(angle, 0.0, 360.0);
+}
+
+float from180to180(float &angle){
+  return limit_angle(angle, -180.0, 180.0);
+}
+
+float limit_angle(float &angle, float mi, float ma){
+  while(angle > ma) angle -= 360;
+  while(angle < mi) angle += 360;
+  return angle;
+}
+
