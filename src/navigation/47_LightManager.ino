99221de77f617a5604e066e4b9f4e52642532012
ElieCo <elieco96@gmail.com>
2020-03-08 17:55:16 +0100

    implement light manager

--- src/navigation/47_LightManager.ino
+++ src/navigation/47_LightManager.ino
@@ -2,6 +2,24 @@
 
 class LightManager : public BaseManager
 {
+
+  void init(){
+    m_btn_led.init(13);
+  }
   
+  void go(){
+    bool gps_ready;
+    if (m_db->getData("Gps_ready", gps_ready)){
+      if (gps_ready) {
+        m_btn_led.on();
+      } else {
+        m_btn_led.reverse();
+      }
+    }
+  }
+
+  private:
+
+  Led m_btn_led;
 };
 
