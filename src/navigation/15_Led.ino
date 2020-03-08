19c44b8e7136d39962f9133b5628a65719928d10
ElieCo <elieco96@gmail.com>
2020-03-08 17:54:14 +0100

    add reverse function for led

--- src/navigation/15_Led.ino
+++ src/navigation/15_Led.ino
@@ -10,18 +10,28 @@ class Led
   void init(int pin){
     pinMode(pin, OUTPUT);
     m_pin = pin;
+    m_state = false;
   }
 
   void on(){
     digitalWrite(m_pin, HIGH);
+    m_state = true;
   }
   
   void off(){
     digitalWrite(m_pin, LOW);
+    m_state = false;
+  }
+
+  void reverse(){
+    if (m_state) digitalWrite(m_pin, LOW);
+    else digitalWrite(m_pin, HIGH);
+    m_state = !m_state;
   }
 
   private:
 
+  bool m_state;
   int m_pin;
 };
 
