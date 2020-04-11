17cc330948f6621f66e501d618f9c3a6299f53f7
ElieCo <elieco96@gmail.com>
2020-04-11 17:41:13 +0200

    change comm protocol

--- src/navigation/45_CommManager.ino
+++ src/navigation/45_CommManager.ino
@@ -6,22 +6,59 @@ class CommManager : public BaseManager
   BASIC_CONSTRUCTOR(CommManager)
   ~CommManager(){}
 
+  void init(){
+    m_lora.init();
+
+    m_index_log = 0;
+  }
+
   void go(){
     // check received messages
     String msg_rcv = m_lora.receive();
+    if (msg_rcv.length() <= 0) return;
     m_db->setData("Msg_received", msg_rcv);
 
     // send message
-    std::map<string, string> data = m_db->getAllData();
-    for (std::map<string, string>::iterator it = data.begin(); it != data.end(); ++it){
-      String var_name = it->first.c_str();
-      String var_value = it->second.c_str();
-      char separ = 0x1F;
-      String msg = var_name + separ + var_value;
-
-      m_lora.send(msg);
+    bool send_log = msg_rcv.indexOf("log")>= 0;
+    if (send_log) {
+      if (m_index_log == 0){
+        m_actual_line = getLine();
+        m_index_log++;
+        sendLog(m_actual_line.substring(0,50), false);
+      }
+      else {
+        m_index_log = 0;
+        send_log = false;
+        sendLog(m_actual_line.substring(50), true);
+      }
     }
   }
 
+  private:
+
+  String getLine(){
+    std::map<string, string> data = m_db->getAllData();
+    String line = "";
+    for (std::map<string, string>::iterator it = data.begin(); it != data.end(); ++it){
+      line += it->second.c_str();
+      if (it != data.end()) {
+        line += ";";
+      }
+    }
+    return line;
+  }
+
+  void sendLog(String msg, boolean end_communication)
+  {
+    if (end_communication){
+      msg = msg + '~';
+    }
+    
+    m_lora.send(msg);
+  }
+
+
   LoRa m_lora;
+  int m_index_log;
+  String m_actual_line;
 };
