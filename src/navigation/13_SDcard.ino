f9f342f5e5dfae39895ab506dc225167bef1254b
ElieCo <elieco96@gmail.com>
2020-04-11 17:02:39 +0200

    make sure that the file is opened to write into it

--- src/navigation/13_SDcard.ino
+++ src/navigation/13_SDcard.ino
@@ -4,23 +4,29 @@ class SDcard
 {
   public:
 
-  SDcard(){}
+  SDcard(){
+    m_file_opened = false;
+  }
   ~SDcard(){
     m_file.close();
   }
 
   void initLog(){
 
-    if (!SD.begin(BUILTIN_SDCARD)) {
-      Serial1.println("initialization carte SD : failed");
+    if (SD.begin(BUILTIN_SDCARD)) {
+      Serial1.println("initialization carte SD : OK");
+      m_file = SD.open("log.csv", FILE_WRITE);
+      m_file_opened = true;
     }
     else {
-      Serial1.println("initialization carte SD : OK");
+      Serial1.println("initialization carte SD : failed");
+      m_file_opened = false;
     }
-    m_file = SD.open("log.csv", FILE_WRITE);
   }
 
   void log(String line){
+    if (!m_file_opened) return;
+
     m_file.println(line);
     m_file.flush();
   }
@@ -30,4 +36,5 @@ class SDcard
   }
 
   File m_file;
+  bool m_file_opened;
 };
