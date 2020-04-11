36657d56c7385b4d38514607cc6bccf285d7bd85
ElieCo <elieco96@gmail.com>
2020-04-11 15:17:25 +0200

    add a security on limit_angle

--- src/navigation/00_Utils.ino
+++ src/navigation/00_Utils.ino
@@ -15,16 +15,17 @@ void print(String s1, String s2){
 }
 
 float from0to360(float &angle){
-  return limit_angle(angle, 0.0, 360.0);
+  return limit_angle(angle, 360.0);
 }
 
 float from180to180(float &angle){
-  return limit_angle(angle, -180.0, 180.0);
+  return limit_angle(angle, 180.0);
 }
 
-float limit_angle(float &angle, float mi, float ma){
+float limit_angle(float &angle, float ma){
+
+  float mi = ma - 360;
   while(angle > ma) angle -= 360;
   while(angle < mi) angle += 360;
   return angle;
 }
-
