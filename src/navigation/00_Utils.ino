a2e6558fd75852474923b31fa330fedac71f71df
ElieCo <elieco96@gmail.com>
2020-04-13 16:45:41 +0200

    add speed conversion

--- src/navigation/00_Utils.ino
+++ src/navigation/00_Utils.ino
@@ -14,6 +14,14 @@ void print(String s1, String s2){
   serialDebug.println(s2);
 }
 
+float toMS(float speed){
+  return speed/1.94384;
+}
+
+float toKnots(float speed){
+  return speed*1.94384;
+}
+
 /**
  * Move the angle in a interval of 360 deg.
  * @param angle a reference to the angle.
