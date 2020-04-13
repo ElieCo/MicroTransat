2348e51eb15dd6a01f9a2278a778430138921419
ElieCo <elieco96@gmail.com>
2020-04-13 15:59:08 +0200

    don't return value of the angle

--- src/navigation/00_Utils.ino
+++ src/navigation/00_Utils.ino
@@ -15,32 +15,28 @@ void print(String s1, String s2){
 }
 
 /**
+ * Move the angle in a interval of 360 deg.
+ * @param angle a reference to the angle.
+ * @param ma the superior limit.
+ */
+void limit_angle(float &angle, float ma){
+  float mi = ma - 360;
+  while(angle > ma) angle -= 360;
+  while(angle < mi) angle += 360;
+}
+
+/**
  * Move the angle between 0 and 360 deg.
  * @param angle a reference to the angle.
- * @return angle the new angle.
  */
-float from0to360(float &angle){
-  return limit_angle(angle, 360.0);
+void from0to360(float &angle){
+  limit_angle(angle, 360.0);
 }
 
 /**
  * Move the angle between -180 and 180 deg.
  * @param angle a reference to the angle.
- * @return angle the new angle.
  */
-float from180to180(float &angle){
-  return limit_angle(angle, 180.0);
-}
-
-/**
- * Move the angle in a interval of 360 deg.
- * @param angle a reference to the angle.
- * @param ma the superior limit.
- * @return the new angle.
- */
-float limit_angle(float &angle, float ma){
-  float mi = ma - 360;
-  while(angle > ma) angle -= 360;
-  while(angle < mi) angle += 360;
-  return angle;
+void from180to180(float &angle){
+  limit_angle(angle, 180.0);
 }
