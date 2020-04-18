05be4642e219b6e14b9dc9c31889609f08234be8
ElieCo <elieco96@gmail.com>
2020-04-18 21:08:39 +0200

    why?

--- src/navigation/01_GeoTools.ino
+++ src/navigation/01_GeoTools.ino
@@ -9,17 +9,17 @@ float get_distance(double lat1, double lng1, double lat2, double lng2){
   return TinyGPS::distance_between(lat1, lng1, lat2, lng2);
 }
 
-float get_distance(Coord coord1, Coord coord2){
-  return get_distance(coord1.lat, coord1.lng, coord2.lat, coord2.lng);
-}
+//float get_distance(Coord coord1, Coord coord2){
+//  return get_distance(coord1.lat, coord1.lng, coord2.lat, coord2.lng);
+//}
 
 float get_course(double lat1, double lng1, double lat2, double lng2){
   return TinyGPS::course_to(lat1, lng1, lat2, lng2);
 }
 
-float get_course(Coord coord1, Coord coord2){
-  return get_course(coord1.lat, coord1.lng, coord2.lat, coord2.lng);
-}
+//float get_course(Coord coord1, Coord coord2){
+//  return get_course(coord1.lat, coord1.lng, coord2.lat, coord2.lng);
+//}
 
 /**
  * Find the coordinates of the point thanks to an other point,
@@ -40,6 +40,6 @@ void getPointAtDistAndBearing(double actual_lat, double actual_lon, double dista
   new_lon = degrees(new_lon);
 }
 
-void getPointAtDistAndBearing(Coord actual_coord, double distance, double bearing, Coord &new_coord) {
-  getPointAtDistAndBearing(actual_coord.lat, actual_coord.lng, distance, bearing, new_coord.lat, new_coord.lng);
-}
+//void getPointAtDistAndBearing(Coord actual_coord, double distance, double bearing, Coord &new_coord) {
+//  getPointAtDistAndBearing(actual_coord.lat, actual_coord.lng, distance, bearing, new_coord.lat, new_coord.lng);
+//}
