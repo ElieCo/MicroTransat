8ad9c4388f56fa676ff413964d9fd11dfdd57c77
ElieCo <elieco96@gmail.com>
2020-05-21 01:29:09 +0200

    use an analog average

--- src/navigation/16_PwmReader.ino
+++ src/navigation/16_PwmReader.ino
@@ -5,17 +5,16 @@ public:
   PwmReader(){}
   ~PwmReader(){}
   
-  void init(int pin, int min_average = 55, int max_average = 95, int precision = 1000){
+  void init(int pin, int min_val = 55, int max_val = 95){
     pinMode(pin, INPUT);
     m_pin = pin;
-    m_min_average = min_average;
-    m_max_average = max_average;
-    m_average.init(precision);
+    m_min_val = min_val;
+    m_max_val = max_val;
   }
 
   float updateValue(){
-    float val = m_average.average(analogRead(m_pin));
-    float pct = 100 * (val - m_min_average)/(m_max_average - m_min_average);
+    float val = analogRead(m_pin);
+    float pct = 100 * (val - m_min_val)/(m_max_val - m_min_val);
     pct = pct < 0 ? 0 : pct;
     pct = pct > 100 ? 100 : pct;
     return pct;
@@ -23,7 +22,6 @@ public:
 
 private:
   int m_pin;
-  float m_min_average;
-  float m_max_average;
-  Average<int> m_average;
+  float m_min_val;
+  float m_max_val;
 };
